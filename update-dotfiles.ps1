67aa3b6a10a53a612104ed6bae45e632f2bb9454
Mark Vincze <mrk.vincze@gmail.com>
2025-10-09 21:06:14 +0200

    Update

--- update-dotfiles.ps1
+++ update-dotfiles.ps1
@@ -1,2 +1,6 @@
 cp C:\Users\markv\AppData\Local\nvim\* .\AppData\Local\nvim -Recurse
 cp C:\Users\markv\.glzr\glazewm\config.yaml .\.glzr\glazewm\
+
+git add .
+git commit -m "Update"
+git push origin main
