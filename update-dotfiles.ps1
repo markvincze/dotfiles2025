c1b06070212706b48aff5908e0466321fc7b81ae
Mark Vincze <mrk.vincze@gmail.com>
2025-10-22 23:30:27 +0200

    Update

--- update-dotfiles.ps1
+++ update-dotfiles.ps1
@@ -2,8 +2,9 @@ cp C:\Users\markv\AppData\Local\nvim\* .\AppData\Local\nvim -Recurse -Force
 cp C:\Users\markv\.glzr\glazewm\config.yaml .\.glzr\glazewm\ -Force
 cp C:\Users\markv\.glzr\zebar\settings.json .\.glzr\zebar\settings.json -Force
 cp C:\Users\markv\.glzr\zebar\dunebar .\.glzr\zebar\dunebar -Recurse -Force
+rm -rf .\.glzr\zebar\dunebar\.git
 cp $PROFILE .
 
-# git add .
-# git commit -m "Update"
-# git push origin main
+git add .
+git commit -m "Update"
+git push origin main
